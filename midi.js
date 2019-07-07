a83d907616eeb9f9ff524cd91ccb6ee29eae92c7
henry.withers <henry.withers@ivendi.com>
2019-07-07 13:01:44 +0100

    load jzz module

--- midi.js
+++ midi.js
@@ -26,79 +26,93 @@ displayNumberOfNotes();
 attachAccompanimentTypeHandlers();
 attachMidiChannelChangeHandlers();
 
-WebMidi.enable(function(err) {
-  if (err) {
-    console.log("WebMidi could not be enabled.", err);
-  } else {
-    console.log("WebMidi enabled!");
+const input = JZZ()
+  .openMidiIn()
+  .or("Cannot start MIDI engine!")
+  .and(function() {
+    console.log("MIDI-in ", this.name());
+  });
 
-    let input = WebMidi.getInputByName("EIE");
-    let output = WebMidi.getOutputByName("EIE");
+const output = JZZ()
+  .or("Cannot start MIDI engine!")
+  .openMidiOut()
+  .or("Cannot open MIDI Out port!");
 
-    console.log(input);
-    input.addListener("noteon", inputChannel, function(e) {
-      console.log(
-        "Received 'noteon' message (" + e.note.name + e.note.octave + ").",
-        e.note.number
-      );
+// JZZ().or("blah");
 
-      notesPlayed.push(e.note.number % 12);
+// JZZ.enable(function(err) {
+//   if (err) {
+//     console.log("WebMidi could not be enabled.", err);
+//   } else {
+//     console.log("WebMidi enabled!");
 
-      const last10Notes = notesPlayed.slice(
-        -numberOfNotesToConsider,
-        notesPlayed.length
-      );
+//     let input = JZZ.getInputByName("EIE");
+//     let output = JZZ.getOutputByName("EIE");
 
-      const possibleNotes = getPotentialNotes(last10Notes);
+//     console.log(input);
+//     input.addListener("noteon", inputChannel, function(e) {
+//       console.log(
+//         "Received 'noteon' message (" + e.note.name + e.note.octave + ").",
+//         e.note.number
+//       );
 
-      if (notesPlayed.length > 5) {
-        const randomNote =
-          possibleNotes[Math.ceil(Math.random() * (possibleNotes.length - 1))];
+//       notesPlayed.push(e.note.number % 12);
 
-        console.log("random note", randomNote);
+//       const last10Notes = notesPlayed.slice(
+//         -numberOfNotesToConsider,
+//         notesPlayed.length
+//       );
 
-        const randomOctave = Math.ceil(Math.random() * 2);
+//       const possibleNotes = getPotentialNotes(last10Notes);
 
-        const notePlusInterval = e.note.number + interval;
-        let noteToPlay = scaleWithMostMatches.includes(notePlusInterval % 12)
-          ? e.note.number + interval
-          : notePlusInterval + 1;
+//       if (notesPlayed.length > 5) {
+//         const randomNote =
+//           possibleNotes[Math.ceil(Math.random() * (possibleNotes.length - 1))];
 
-        if (accompanimentTypePreference === "random") {
-          // play a random note from notes already played
-          console.log("will play note", randomNote + 48 + randomOctave * 12);
+//         console.log("random note", randomNote);
 
-          output.playNote(randomNote + 48 + randomOctave * 12, outputChannel, {
-            time: WebMidi.time + 10,
-            duration: 500,
-            velocity: 0.75
-          });
-        } else if (accompanimentTypePreference === "other") {
-          // play a random note from scale with most matches from last n notes
-          console.log("will play note", randomNoteFromScaleWithMostMatches);
+//         const randomOctave = Math.ceil(Math.random() * 2);
 
-          output.playNote(
-            randomNoteFromScaleWithMostMatches + 48 + randomOctave * 12,
-            outputChannel,
-            {
-              time: WebMidi.time + 30,
-              duration: 500,
-              velocity: 0.75
-            }
-          );
-        } else if (accompanimentTypePreference === "harmony") {
-          // play harmony 3 or 4 semitones above
-          console.log("will play note", noteToPlay);
-          output.playNote(noteToPlay, outputChannel, {
-            time: WebMidi.time + 10,
-            duration: 500,
-            velocity: 0.75
-          });
-        }
-      }
-    });
-  }
-});
+//         const notePlusInterval = e.note.number + interval;
+//         let noteToPlay = scaleWithMostMatches.includes(notePlusInterval % 12)
+//           ? e.note.number + interval
+//           : notePlusInterval + 1;
+
+//         if (accompanimentTypePreference === "random") {
+//           // play a random note from notes already played
+//           console.log("will play note", randomNote + 48 + randomOctave * 12);
+
+//           output.playNote(randomNote + 48 + randomOctave * 12, outputChannel, {
+//             time: WebMidi.time + 10,
+//             duration: 500,
+//             velocity: 0.75
+//           });
+//         } else if (accompanimentTypePreference === "other") {
+//           // play a random note from scale with most matches from last n notes
+//           console.log("will play note", randomNoteFromScaleWithMostMatches);
+
+//           output.playNote(
+//             randomNoteFromScaleWithMostMatches + 48 + randomOctave * 12,
+//             outputChannel,
+//             {
+//               time: WebMidi.time + 30,
+//               duration: 500,
+//               velocity: 0.75
+//             }
+//           );
+//         } else if (accompanimentTypePreference === "harmony") {
+//           // play harmony 3 or 4 semitones above
+//           console.log("will play note", noteToPlay);
+//           output.playNote(noteToPlay, outputChannel, {
+//             time: WebMidi.time + 10,
+//             duration: 500,
+//             velocity: 0.75
+//           });
+//         }
+//       }
+//     });
+//   }
+// });
 
 let startButton = document.getElementById("start");
 startButton.onclick = function() {
@@ -221,7 +235,7 @@ function attachMidiChannelChangeHandlers() {
   inputChannelEle.onclick = function(e) {
     inputChannel = +e.target.value;
   };
-  const inputChannelEle = document.getElementById("outputChannelSelect");
+  const outputChannelEle = document.getElementById("outputChannelSelect");
   outputChannelEle.onclick = function(e) {
     outputChannel = +e.target.value;
   };
